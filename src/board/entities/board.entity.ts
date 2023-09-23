976be66dcf1cf0b04cb16d5d2939dd2b2c2c0be1
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-09-23 20:48:38 +0900

    typeorm을 이용한 게시판 Entity 생성

---
+++ src/board/entities/board.entity.ts
@@ -0,0 +1,42 @@
+import { BoardImage } from "src/board_image/entities/board_image.entity";
+import { Community } from "src/community/entities/community.entity";
+import { User } from "src/user/entities/user.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Board {
+    @PrimaryGeneratedColumn({name:'board_id'})
+    id:number;
+
+    @Column({type:'varchar',length:45})
+    title:string;
+
+    @Column({type:'varchar',length:1000})
+    content:string;
+
+    @Column({type:'tinyint'})
+    views:number;
+
+    @Column({type: 'tinyint'})
+    likes: number;
+
+    @CreateDateColumn({name:'created_at'})
+    date:Date;
+
+    @Column({type:'tinyint'})
+    visible:number;
+
+    @Column({type:'tinyint'})
+    completion:number;
+
+    @ManyToOne(()=>User, (user)=>user.id)
+    @JoinColumn({name:'user_id'})
+    user:User;
+
+    @ManyToOne(()=>Community, (community)=>community.id)
+    @JoinColumn({name:'community_id'})
+    community:Community;
+
+    @OneToMany(()=>BoardImage, (boardImage)=>boardImage.board)
+    boardImages: BoardImage[];
+}
